f002fd0f8c19fe8899683d0b886983040096c49a
Karan Goel <karan@goel.im>
2014-12-11 15:58:40 -0800

    show buttons on hover on page title

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,20 @@
 $(function() {
 
+  function addPageToList(title) {
+    $('.page_list').append('<div class="existing_page">' + title +
+          '<a class="delete_button"><a class="edit_button"></div>');
+  }
+
   $.get('/api/pages', function(data) {
     for (var page in data) {
-      $('.page_list').append('<div class="existing_page">' + data[page].page.title +
-        '</div>');
+      addPageToList(data[page].page.title);
     }
   });
 
   $('.add_button').click(function(e) {
     $.post('/api/pages', { title: $('.page_title').val(), user_id: user_id },
       function(data) {
-        $('.page_list').append('<div class="existing_page">' + data.page.title +
-          '</div>');
+        addPageToList(data.page.title);
       });
   });
 
